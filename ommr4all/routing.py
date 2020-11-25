6f9fe2028eb82677d636584619e0f9ffcd601ed2
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2020-11-25 15:56:01 +0100

    Fixed ASGI setup.

--- ommr4all/routing.py
+++ ommr4all/routing.py
@@ -1,5 +1,7 @@
 from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'http': get_asgi_application(),
 })

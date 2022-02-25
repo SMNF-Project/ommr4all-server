e3706a213295c4630e2e546d197f4329b617ede3
Jan Hajic Jr <hajicj@ufal.mff.cuni.cz>
2022-02-25 10:31:19 +0100

    Removed German-lanugage version to bypass i18n issues conencted to updating Angular.

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, re_path
+from django.contrib import admin
 import webapp.views as views
 
 urlpatterns = \
     [
         path('', views.index),
+        path('admin/', admin.site.urls),
         re_path(r'^(?P<path>.*)/$', views.index, name='index'),
     ]
